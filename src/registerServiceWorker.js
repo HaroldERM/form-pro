475674e1d39478410960bc7f5581cee65bf8727e
Harold <enrique5129876@estudiantec.cr>
2023-11-15 20:29:41 -0600

    SW changes

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from "register-service-worker";
+import { Workbox } from "workbox-window";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -21,6 +22,8 @@ if (process.env.NODE_ENV === "production") {
     },
     updated() {
       console.log("New content is available; please refresh.");
+      // Reload the page to apply the update
+      window.location.reload(true);
     },
     offline() {
       console.log(
@@ -31,4 +34,32 @@ if (process.env.NODE_ENV === "production") {
       console.error("Error during service worker registration:", error);
     },
   });
+
+  // Check if Workbox is supported by the browser
+  if ("serviceWorker" in navigator) {
+    // Create a new instance of Workbox
+    const workbox = new Workbox(`${process.env.BASE_URL}service-worker.js`);
+
+    // Event triggered when the installed service worker takes control
+    workbox.addEventListener("controlling", () => {
+      window.location.reload();
+    });
+
+    // Event triggered when there's a new service worker available
+    workbox.addEventListener("waiting", () => {
+      if (confirm("New version available! Do you want to refresh?")) {
+        // Notify the new service worker that it can take control
+        workbox.messageSW({ type: "SKIP_WAITING" });
+      }
+    });
+
+    // Register the service worker and manage the cache
+    workbox.register();
+
+    // Configurar la estrategia de caché para el index.html
+    workbox.routing.registerRoute(
+      ({ request }) => request.destination === "document",
+      new workbox.strategies.CacheFirst()
+    );
+  }
 }
